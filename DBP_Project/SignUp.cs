6dc892827c2b6d28fe79e556dc6e30fc0cc8fe8e
박은빈 <corwoddl@naver.com>
2022-11-04 11:57:21 +0900

    EunBin
    SignUp.cs UI

--- DBP_Project/SignUp.cs
+++ DBP_Project/SignUp.cs
@@ -16,5 +16,15 @@ namespace DBP_Project
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
