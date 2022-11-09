48ee01a3e6c4c3799e1ffb09597a92969899ef91
Henry-Cha <sungwon4728@naver.com>
2022-11-09 17:41:30 +0900

    관리자 ui

---
+++ DBP_Project/ManagerForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBP_Project
+{
+    public partial class ManagerForm : Form
+    {
+        public ManagerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
