0f97fa22406afd2387b2d154ecb3d802a6d4331e
qoridhc <67674337+qoridhc@users.noreply.github.com>
2022-11-23 12:02:11 +0900

    Update UserManagerForm.cs

--- DBP_Project/UserManagerForm.cs
+++ DBP_Project/UserManagerForm.cs
@@ -50,7 +50,7 @@ namespace DBP_Project
         private void serachBtn_Click(object sender, EventArgs e) // 유저 검색 버튼을 눌렀을 경우
         {
             string searchName = searchBox.Text;
-            string searchByNameQuery = $"SELECT * FROM Department Left outer Join LogInHistory on Department.userId = LogInHistory.userId where Department.userId = '{searchName}'";
+            string searchByNameQuery = $"SELECT * FROM UserDepartment Left outer Join LogInHistory on UserDepartment.userId = LogInHistory.userId where UserDepartment.userId = '{searchName}'";
 
             if (searchName == "") // 아무런 id도 입력하지않고 검색을 누른경우
             {
@@ -80,7 +80,7 @@ namespace DBP_Project
             getUserList();
         }
 
-        private void departmentComboBox_SelectedIndexChanged(object sender, EventArgs e) // 
+        private void departmentComboBox_SelectedIndexChanged(object sender, EventArgs e) // 사용자 부서 콤보 박스 인덱스 변경시
         {
             string currentDepartment = departmentComboBox.SelectedItem.ToString();
             string getTeamInfoQuery = $"select distinct teamName from departmentList where departmentName = '{currentDepartment}'";
@@ -93,23 +93,23 @@ namespace DBP_Project
             }
         }
 
-        private void saveBtn_Click(object sender, EventArgs e)
+        private void saveBtn_Click(object sender, EventArgs e) // 변경사항 저장 버튼 눌렀을 경우
         {
-            if (departmentComboBox.SelectedItem == null)
+            if (departmentComboBox.SelectedItem == null)  // 예외처리
                 MessageBox.Show("소속 부서를 선택해주세요");
             else if (teamComboBox.SelectedItem == null)
                 MessageBox.Show("소속 팀을 선택해주세요");
             else
             {
-                string currentUserId = userBox.Text;
+                string currentUserId = userBox.Text; // 사용자의 부서와 팀 정상적으로 선택하면 DB에 업데이트
                 string currentDepartment = departmentComboBox.SelectedItem.ToString();
                 string currentTeam = teamComboBox.SelectedItem.ToString();
-                string saveInfoQuery = $"UPDATE Department SET departmentName = '{currentDepartment}', teamName = '{currentTeam}' where userId = '{currentUserId}'";
+                string saveInfoQuery = $"UPDATE UserDepartment SET departmentName = '{currentDepartment}', teamName = '{currentTeam}' where userId = '{currentUserId}'";
                 Query.GetInstance().RunQuery(saveInfoQuery);
             }
         }
 
-        private void getBlockDepartmentListByUid(string currentUserId)
+        private void getBlockDepartmentListByUid(string currentUserId) // UserId로 차단 부서 정보 받아오는 쿼리 실행
         {
             string getBlockDepartmentQuery = $"SELECT blockDepartment FROM BlockInfo where userId = '{currentUserId}' and blockDepartment is not null;";
             DataTable blockDepartmentTable = Query.GetInstance().RunQuery(getBlockDepartmentQuery);
@@ -122,15 +122,15 @@ namespace DBP_Project
             }
         }
 
-        private void blockAddBtn1_Click(object sender, EventArgs e)
+        private void blockAddBtn1_Click(object sender, EventArgs e) // 차단 부서에 새로운 부서 추가
         {
-            if (userBox.Text == "")
+            if (userBox.Text == "") // 사용자 조회를 하지 않고 부서 추가 하는 경우 예외 처리
             {
                 MessageBox.Show("사용자를 먼저 검색해 주세요");
                 return;
             }
 
-            if (blockComboBox1.SelectedItem == null)
+            if (blockComboBox1.SelectedItem == null) // 차단할 부서를 선택하지 않고 차단부서 추가를 누른경우 예외 처리
             {
                 MessageBox.Show("보기를 차단할 부서를 선택해 주세요");
                 return;
@@ -138,10 +138,11 @@ namespace DBP_Project
 
             string currentUserId = userBox.Text;
             string seletctedDepartment = blockComboBox1.SelectedItem.ToString();
-            if (blockDepartmentList.Items.Contains(seletctedDepartment))
+            if (blockDepartmentList.Items.Contains(seletctedDepartment)) // 이미 차단 목록에 있는 부서를 다시 차단하려는 경우 예외 처리
                 MessageBox.Show("이미 차단한 부서입니다.");
             else
             {
+                // 정상적으로 차단할 부서를 추가한경우 차단테이블 추가 DB 쿼리 실행
                 string updateBlockListQuery = $"insert into BlockInfo(userId, blockDepartment) Value('{currentUserId}', '{seletctedDepartment}')";
                 Query.GetInstance().RunQuery(updateBlockListQuery);
 
@@ -149,7 +150,7 @@ namespace DBP_Project
             }
         }
 
-        private void blockDeleteBtn1_Click(object sender, EventArgs e)
+        private void blockDeleteBtn1_Click(object sender, EventArgs e) // 차단 부서 제거 버튼 눌렀을 경우
         {
             if (blockComboBox1.SelectedItem == null)
                 MessageBox.Show("차단목록에서 삭제할 부서를 선택해주세요");
@@ -176,7 +177,7 @@ namespace DBP_Project
             blockComboBox1.SelectedIndex = blockComboBox1.FindString(selectedDepartment);
         }
 
-        private void getBlockChatListByUid(string currentUserId)
+        private void getBlockChatListByUid(string currentUserId) // UserId로 DB에서 채팅 차단 정보를 가져오는 함수
         {
             string getBlockChatListQuery = $"SELECT blockUserId FROM BlockInfo where userId = '{currentUserId}' and blockType = 2;";
             DataTable blockChatTable = Query.GetInstance().RunQuery(getBlockChatListQuery);
@@ -189,7 +190,7 @@ namespace DBP_Project
             }
         }
 
-        private void addBlockChatBtn_Click(object sender, EventArgs e)
+        private void addBlockChatBtn_Click(object sender, EventArgs e) // 채팅 차단하고자하는 사용자 추가하는 버튼
         {
             if (userBox.Text == "")
             {
@@ -216,7 +217,7 @@ namespace DBP_Project
             }
         }
 
-        private void deleteBlockChatBtn_Click(object sender, EventArgs e)
+        private void deleteBlockChatBtn_Click(object sender, EventArgs e) // 채팅 차단 목록에 추가되어있는 유저 삭제 하는 버튼
         {
             if (blockChatBox.SelectedItem == null)
                 MessageBox.Show("차단목록에서 삭제할 사용자ID를 선택해주세요");
@@ -244,7 +245,7 @@ namespace DBP_Project
             blockChatBox.SelectedIndex = blockChatBox.FindString(selectedUserId);
         }
 
-        private void getBlockUserListByUid(string currentUserId)
+        private void getBlockUserListByUid(string currentUserId) // UserId로 사용자 보기 차단 리스트를 가져오는 함수
         {
             string getBlockUserListQuery = $"SELECT blockUserId FROM BlockInfo where userId = '{currentUserId}' and blockType = 1;";
             DataTable blockUserTable = Query.GetInstance().RunQuery(getBlockUserListQuery);
@@ -257,7 +258,7 @@ namespace DBP_Project
             }
         }
 
-        private void addBlockUserBtn_Click(object sender, EventArgs e)
+        private void addBlockUserBtn_Click(object sender, EventArgs e) // 보기 차단 유저 추가 버튼
         {
             if (userBox.Text == "")
             {
@@ -284,7 +285,7 @@ namespace DBP_Project
             }
         }
 
-        private void deleteBlockUserBtn_Click(object sender, EventArgs e)
+        private void deleteBlockUserBtn_Click(object sender, EventArgs e) // 보기 차단 유저 삭제
         {
             if (blockUserBox.SelectedItem == null)
                 MessageBox.Show("보기차단목록에서 삭제할 사용자ID를 선택해주세요");
@@ -320,6 +321,5 @@ namespace DBP_Project
         {
             blockUserPanel.Visible = false;
         }
-
     }
 }
