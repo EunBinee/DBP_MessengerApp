653fc2fb58ece2e86a9877baa61d42e3fcd866b0
Falcon <royale@kakao.com>
2022-12-05 16:37:19 +0900

    Fix : 채팅방 비율 조절

--- DBP_Project/Chat.Designer.cs
+++ DBP_Project/Chat.Designer.cs
@@ -56,29 +56,27 @@
             this.panel2.Controls.Add(this.button3);
             this.panel2.Controls.Add(this.button2);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 902);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel2.Location = new System.Drawing.Point(0, 483);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(540, 156);
+            this.panel2.Size = new System.Drawing.Size(378, 104);
             this.panel2.TabIndex = 2;
             // 
             // msgInput
             // 
             this.msgInput.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.msgInput.Location = new System.Drawing.Point(123, 16);
-            this.msgInput.Margin = new System.Windows.Forms.Padding(4);
+            this.msgInput.Location = new System.Drawing.Point(86, 11);
             this.msgInput.Multiline = false;
             this.msgInput.Name = "msgInput";
-            this.msgInput.Size = new System.Drawing.Size(283, 120);
+            this.msgInput.Size = new System.Drawing.Size(199, 81);
             this.msgInput.TabIndex = 3;
             this.msgInput.Text = "";
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(416, 16);
-            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Location = new System.Drawing.Point(291, 11);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(107, 122);
+            this.button5.Size = new System.Drawing.Size(75, 81);
             this.button5.TabIndex = 3;
             this.button5.Text = "전송";
             this.button5.UseVisualStyleBackColor = true;
@@ -86,10 +84,9 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(7, 104);
-            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Location = new System.Drawing.Point(5, 69);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(107, 34);
+            this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 2;
             this.button4.Text = "파일";
             this.button4.UseVisualStyleBackColor = true;
@@ -97,10 +94,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(7, 60);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(5, 40);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(107, 34);
+            this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 1;
             this.button3.Text = "사진";
             this.button3.UseVisualStyleBackColor = true;
@@ -108,10 +104,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(7, 16);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(5, 11);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(107, 34);
+            this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 0;
             this.button2.Text = "이모티콘";
             this.button2.UseVisualStyleBackColor = true;
@@ -122,18 +117,18 @@
             this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.Controls.Add(this.Emptypanel4);
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 81);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 54);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(540, 820);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(378, 547);
             this.flowLayoutPanel1.TabIndex = 3;
             this.flowLayoutPanel1.WrapContents = false;
             // 
             // Emptypanel4
             // 
-            this.Emptypanel4.Location = new System.Drawing.Point(3, 3);
+            this.Emptypanel4.Location = new System.Drawing.Point(2, 2);
+            this.Emptypanel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Emptypanel4.Name = "Emptypanel4";
-            this.Emptypanel4.Size = new System.Drawing.Size(534, 110);
+            this.Emptypanel4.Size = new System.Drawing.Size(374, 73);
             this.Emptypanel4.TabIndex = 0;
             // 
             // label1
@@ -142,20 +137,19 @@
             this.label1.Cursor = System.Windows.Forms.Cursors.SizeAll;
             this.label1.Font = new System.Drawing.Font("한컴산뜻돋움", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label1.ForeColor = System.Drawing.SystemColors.Window;
-            this.label1.Location = new System.Drawing.Point(17, 18);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(307, 43);
+            this.label1.Size = new System.Drawing.Size(208, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "데이터베이스 채팅방";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("한컴산뜻돋움", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button1.Location = new System.Drawing.Point(449, 16);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Location = new System.Drawing.Point(314, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(74, 46);
+            this.button1.Size = new System.Drawing.Size(52, 31);
             this.button1.TabIndex = 3;
             this.button1.Text = "고정";
             this.button1.UseVisualStyleBackColor = true;
@@ -163,10 +157,9 @@
             // 
             // panel3
             // 
-            this.panel3.Location = new System.Drawing.Point(0, 86);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Location = new System.Drawing.Point(0, 57);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(540, 810);
+            this.panel3.Size = new System.Drawing.Size(378, 540);
             this.panel3.TabIndex = 3;
             // 
             // panel1
@@ -178,32 +171,32 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(540, 81);
+            this.panel1.Size = new System.Drawing.Size(378, 54);
             this.panel1.TabIndex = 1;
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(337, 18);
+            this.trackBar1.Location = new System.Drawing.Point(236, 12);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.trackBar1.Maximum = 100;
             this.trackBar1.Minimum = 10;
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(105, 69);
+            this.trackBar1.Size = new System.Drawing.Size(73, 45);
             this.trackBar1.TabIndex = 0;
             this.trackBar1.Value = 100;
             this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
             // 
             // Chat
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(540, 1058);
+            this.ClientSize = new System.Drawing.Size(378, 587);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Chat";
             this.Text = "Chat";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Chat_FormClosed_1);
